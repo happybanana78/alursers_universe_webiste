bcd72ca3797b6e5c65b1640e9ee799b315e5e1d4
masterfapper72 <leonardo.mazzoleni1@gmail.com>
2022-09-23 09:59:21 +0200

    minor change

--- resources/views/components/flash-found-message.blade.php
+++ resources/views/components/flash-found-message.blade.php
@@ -4,6 +4,6 @@
     x-transition:leave-start="opacity-100 translate-y-0"
     x-transition:leave-end="opacity-0 translate-y-full"
     x-init="setTimeout(() => foundPop = false, 3000)"
-    class="z-20 text-center text-white p-5 px-20 text-2xl absolute bg-red-700
-    container top-0 -translate-x-1/2 w-1/3 left-1/2">{{session("found")}}</div>
+    class="z-30 text-center text-white p-5 text-2xl absolute bg-red-700
+    container top-0 -translate-x-1/2 w-1/2 md:w-1/3 left-1/2">{{session("found")}}</div>
 @endif
